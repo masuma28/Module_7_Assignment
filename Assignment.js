b6662cfc7a7aa2985974d895d31086cdd7cb0e3c
masuma28 <aktermasuma2024@gmail.com>
2026-05-23 12:48:21 +0600

    Add files via upload

---
+++ Assignment.js
@@ -0,0 +1,47 @@
+// Task 1: Student Information
+
+let studentName = "Masuma";
+let age = 21;
+let courseName = "JavaScript Basics";
+let isEnrolled = true;
+
+console.log("Student Name:", studentName);
+console.log("Age:", age);
+console.log("Course Name:", courseName);
+console.log("Is Enrolled:", isEnrolled);
+
+
+// Task 2: Calculator using Operators
+
+let num1 = 20;
+let num2 = 5;
+
+console.log("Addition:", num1 + num2);
+console.log("Subtraction:", num1 - num2);
+console.log("Multiplication:", num1 * num2);
+console.log("Division:", num1 / num2);
+
+
+// Task 3: Loop Practice
+
+// for loop (1 to 10)
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// while loop (10 to 1)
+let j = 10;
+
+while (j >= 1) {
+    console.log(j);
+    j--;
+}
+
+
+// Task 6: Function Practice
+
+function helloWorld() {
+    return "Hello World";
+}
+
+console.log(helloWorld());
